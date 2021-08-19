a8d3d78f754330b648e021db8680d119b06474e3
Alexandre Gicquel <gicquelalexandre94@gmail.com>
2021-08-19 16:25:08 +0200

    user routes et ctrl ajoutés, auth ajouté mais pas implémenté dans les routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://AlGicquel:sephiroth@cluster0.bpzu0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,5 +22,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
